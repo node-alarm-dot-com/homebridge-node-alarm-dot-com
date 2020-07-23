d784e48f3b3a6f55774eea16722d8deb6e114cbe
ngori <lists@ngori.net>
2020-07-23 08:35:40 -0400

    Bug fix
    bug Fix

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ class ADCPlatform {
                   this.addLight(d)
                 } else if (realDeviceType === 'lock') {
                   this.addLock(d)
-                  else if (realDeviceType === 'garages')
+                  else if (realDeviceType === 'garage')
                 }
                 // add more devices here as available, ie. garage doors, etc
 
