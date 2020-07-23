2a2cb3bc5fdd8d7d5756aa075678249947714bde
ngori <lists@ngori.net>
2020-07-23 09:22:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ class ADCPlatform {
                   this.addLight(d)
                 } else if (realDeviceType === 'lock') {
                   this.addLock(d)
-                } else if (realDeviceType === 'garage-door') {
+                } else if (realDeviceType === 'garages') {
                   this.addGarage(d)
                 }
                 // add more devices here as available, ie. garage doors, etc
