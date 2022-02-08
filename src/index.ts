d76639f0a9bd95f404849d75bce7cb62901a9777
Kevin Wallace <kwallace71@gmail.com>
2022-02-08 10:08:53 -0800

    Fix for non-dimmable lights Issue #90

--- src/index.ts
+++ src/index.ts
@@ -907,7 +907,7 @@ class ADCPlatform implements DynamicPlatformPlugin {
     accessory.context.state = on;
 
     await this.loginSession()
-      .then(res => method(id, res, accessory.context.lightLevel ?? 100))
+      .then(res => method(id, res, accessory.context.lightLevel ?? 100, accessory.context.isDimmer))
       .then(res => res.data)
       .then(light => {
         this.statLightState(accessory, light, callback);
